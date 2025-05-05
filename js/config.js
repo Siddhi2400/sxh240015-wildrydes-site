097ef8d97773b72b031b6238ad10633b06b8cdca
Siddhi2400 <139800974+Siddhi2400@users.noreply.github.com>
2025-05-04 22:33:26 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_GZukZAdon', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '22i230a8qjsuliaiilekdth6t0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_dU3b5qkO8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '156p6ji4kgptcb0dgishukkan9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
